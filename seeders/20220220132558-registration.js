805d343e51bdab3d1634aadd9eb76163f23d2d97
magnae2016 <magnae2016@gmail.com>
2022-02-20 14:04:16 +0900

    Add Base file related to RGIST (Feature #11)

---
+++ seeders/20220220132558-registration.js
@@ -0,0 +1,28 @@
+'use strict';
+const { v4 } = require('uuid');
+require('dotenv').config();
+
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.bulkInsert(
+            'REGISTRATIONS',
+            [
+                {
+                    regist_id: 2,
+                    regist_uuid: v4(),
+                    regist_year: process.env.YEAR,
+                    regist_title: 'KSEF 내구페스티발 참가 신청서',
+                    type_id: 1,
+                    form_id: 2,
+                },
+            ],
+            {}
+        );
+    },
+
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.bulkDelete('REGISTRATIONS', null, {
+            cascade: true,
+        });
+    },
+};
