f7179381e2d74f00ccd8576f892d3f8989989433
magnae2016 <magnae2016@gmail.com>
2023-01-24 13:24:27 +0900

    Add exception for enable registration button

--- public/javascripts/accounts/register_team.js
+++ public/javascripts/accounts/register_team.js
@@ -7,7 +7,11 @@ $(function () {
         var $form = $(this),
             term = $form.serialize(),
             url = '';
-
+        var searchParams = new URLSearchParams(term);
+        if (!searchParams.get('team_id')) {
+            window.alert('관리자가 팀을 등록한 이후, 팀 선택을 진행해주세요.');
+            return;
+        }
         var posting = $.post(url, term);
 
         posting.done(function (data) {
